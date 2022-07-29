e4c4d0ca26a5b219537167fd705b7d6941fe275a
David pequeno Marinho <63377354+David-Marinho@users.noreply.github.com>
2022-07-29 01:00:46 -0300

    adiçao das armas

---
+++ dados/efeitos/feridas.py
@@ -0,0 +1,10 @@
+class feridas():
+    def __init__(self):
+        pass
+
+    def verificar_armadura(inimigo):
+        if inimigo.armadura:
+            return 5
+            
+        else:
+            return 0
---
+++ dados/efeitos/queimadura.py
@@ -0,0 +1,6 @@
+class queimadura():
+    def __init__(self):
+        pass
+
+    def queimar():
+        return -2
