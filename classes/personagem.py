79ba0f14e800668bdf7c58cf9a63229d5b346827
David pequeno Marinho <63377354+David-Marinho@users.noreply.github.com>
2022-08-15 12:55:36 -0300

    update

--- classes/personagem.py
+++ classes/personagem.py
@@ -1,10 +1,41 @@
+from random import randint
 class Personagem:
-    def __init__(self, hp, atk, defesa, mag, def_mag, speed, possui_efeito):
+    def __init__(self, hp, atk, defesa, mag, def_mag, speed, lista_magia, lista_inventario):
         self.hp = hp
         self.atk = atk
         self.defesa = defesa
         self.mag = mag
         self.def_mag = def_mag
         self.speed = speed
-        self.possui_efeito = possui_efeito
-        self.efeitos = list()
+        self.defendendo = False
+        self.lista_magia = lista_magia
+        self.lista_inventario = lista_inventario
+
+    def atacar(self, inimigo_def):
+        return self.atk - inimigo_def
+
+    def defender(self):
+        self.defendendo = True
+
+    def fugir(self):
+        chance = randint(1, 100)
+
+        if chance <= 20:
+            print('fugiu')
+
+        else:
+            print('nao conseguiu fugir')
+
+    def inventario(self):
+        numero = 1
+        for item in self.lista_inventario:
+            print(f'{numero} -> {item["nome"]} ------ {item["desc"]}')
+
+        escolha = int(input('digite um numero: '))
+        #terminar itens antes de continuar
+
+    def finalizar_turno(self):
+        
+
+
+
--- teste.py
+++ teste.py
@@ -1,2 +1,9 @@
+pessoas = [
+    {'nome': 'joao', 'idade': 20},
+    {'nome': 'paula', 'idade': 22},
+    {'nome': 'pedro', 'idade': 15}
+]
 
+for i in pessoas:
+    print(f'{i["nome"]} {i["idade"]}')
 
